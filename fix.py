f94e3317ecebbd30e7dee3fee5fc30f835175675
puku-777 <63917881+puku-777@users.noreply.github.com>
2021-05-29 18:05:19 -0400

    Update fix.py
    correct insertion of effective date into header

--- fix.py
+++ fix.py
@@ -1,6 +1,6 @@
-with open("FIX.txt") as source, open("fix.dat",'a') as dest, input("NASR effective date >>> ") as effdate:
+with open("FIX.txt") as source, open("fix.dat",'a') as dest:
     dest.write(f"""I
-XP740 compliant for fgfs. Extracted using fix.py by TOASTER from 28-Day NASR Subscriptions effective {effdate} from FAA.gov\n\n""")
+XP740 compliant for fgfs. Extracted using fix.py by TOASTER from 28-Day NASR Subscriptions effective """+input("NASR effective date >>> ")+'from FAA.gov\n\n')
     for z in list(source):
         if "FIX1" in z:
             dest.write(f"{'-' if z[78]=='S' else ' '}{int(z[66:68])+int(z[69:71])/60+float(z[72:78])/3600:09.6f} {'-' if z[93]=='W' else ' '}{int(z[80:83])+int(z[84:86])/60+float(z[87:93])/3600:010.6f} {z[4:9]}\n")
